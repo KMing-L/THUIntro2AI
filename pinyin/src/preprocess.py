6cc958af9271dc2fdbb057e4803add4204606019
KaimingLiu <kming_l@163.com>
2023-03-20 17:51:01 +0800

    feat: finish multhreading for preprocessing

--- pinyin/src/preprocess.py
+++ pinyin/src/preprocess.py
@@ -1,11 +1,86 @@
 import json
 import os
 import argparse
+import threading
 from process_raw_dataset import process_raw_word_pinyin, process_raw_sina_data
 from config import *
 
+process_raw_word_pinyin()
 
-def output_counts(ziyuan, force):
+with open(WORD_TABLE, 'r', encoding='utf-8') as f:
+    words = json.load(f)
+with open(PINYIN2WORDS_DICT, 'r', encoding='utf-8') as f:
+    pinyin2words = json.load(f)
+
+count = {}
+binary_count = {}
+optional_count = {}
+first_count = {}
+
+lock = threading.Lock()
+threading_list = []
+
+may_new = [' ', '‘', '“', '《', '》', '：', '；', '-']
+
+
+def count_month(month, ziyuan):
+    print('统计第', month, '月的字出现数量')
+    for sentence in process_raw_sina_data(SINA_DATA % month):
+        is_first = True
+        for idx, word in enumerate(sentence[0]):
+            if sentence[1][idx] == '*':
+                if word in may_new:
+                    is_first = True
+                continue
+            if word in words \
+                    and sentence[1][idx] in pinyin2words \
+                    and word in pinyin2words[sentence[1][idx]]:
+                this_word = word + sentence[1][idx]
+                lock.acquire()
+                count[this_word] = count.get(this_word, 0) + 1
+                lock.release()
+                if is_first:
+                    lock.acquire()
+                    first_count[this_word] = first_count.get(
+                        this_word, 0) + 1
+                    lock.release()
+                    is_first = False
+                if ziyuan == 2:
+                    if idx > 0 \
+                            and sentence[0][idx - 1] in words \
+                            and sentence[1][idx - 1] in pinyin2words \
+                            and sentence[0][idx - 1] in pinyin2words[sentence[1][idx - 1]]:
+                        last_word = sentence[0][idx -
+                                                1] + sentence[1][idx - 1]
+                        lock.acquire()
+                        if last_word not in binary_count:
+                            binary_count[last_word] = {}
+                        binary_count[last_word][this_word] = binary_count[last_word].get(
+                            this_word, 0) + 1
+                        lock.release()
+                elif ziyuan == 3:
+                    if idx > 1 \
+                            and sentence[0][idx - 1] in words \
+                            and sentence[1][idx - 1] in pinyin2words \
+                            and sentence[0][idx - 1] in pinyin2words[sentence[1][idx - 1]] \
+                            and sentence[0][idx - 2] in words \
+                            and sentence[1][idx - 2] in pinyin2words \
+                            and sentence[0][idx - 2] in pinyin2words[sentence[1][idx - 2]]:
+                        last_word = sentence[0][idx -
+                                                1] + sentence[1][idx - 1]
+                        last_last_word = sentence[0][idx -
+                                                     2] + sentence[1][idx - 2]
+                        lock.acquire()
+                        if last_last_word not in optional_count:
+                            optional_count[last_last_word] = {}
+                        if last_word not in optional_count[last_last_word]:
+                            optional_count[last_last_word][last_word] = {}
+                        optional_count[last_last_word][last_word][this_word] = optional_count[last_last_word][last_word].get(
+                            this_word, 0) + 1
+                        lock.release()
+
+
+def preprocess(ziyuan, force):
     if (not force) \
             and os.path.exists(WORD_COUNT) \
             and os.path.exists(FIRST_WORD_COUNT) \
@@ -13,64 +88,13 @@ def output_counts(ziyuan, force):
         print('已经统计过字出现数量，跳过预处理过程。')
         return
 
-    process_raw_word_pinyin()
-
-    with open(WORD_TABLE, 'r', encoding='utf-8') as f:
-        words = json.load(f)
-    with open(PINYIN2WORDS_DICT, 'r', encoding='utf-8') as f:
-        pinyin2words = json.load(f)
-
-    count = {}
-    binary_count = {}
-    optional_count = {}
-    first_count = {}
-
-    may_new = [' ', '‘', '“', '《', '》', '：', '；', '-']
-
     for month in SINA_MONTH:
-        print('统计第', month, '月的字出现数量')
-        for sentence in process_raw_sina_data(SINA_DATA % month):
-            is_first = True
-            for idx, word in enumerate(sentence[0]):
-                if sentence[1][idx] == '*':
-                    if word in may_new:
-                        is_first = True
-                    continue
-                if word in words \
-                    and sentence[1][idx] in pinyin2words \
-                    and word in pinyin2words[sentence[1][idx]]:
-                    this_word = word + sentence[1][idx]
-                    count[this_word] = count.get(this_word, 0) + 1
-                    if is_first:
-                        first_count[this_word] = first_count.get(
-                            this_word, 0) + 1
-                        is_first = False
-                    if ziyuan == 2:
-                        if idx > 0 \
-                                and sentence[0][idx - 1] in words \
-                                and sentence[1][idx - 1] in pinyin2words \
-                                and sentence[0][idx - 1] in pinyin2words[sentence[1][idx - 1]]:
-                            last_word = sentence[0][idx - 1] + sentence[1][idx - 1]
-                            if last_word not in binary_count:
-                                binary_count[last_word] = {}
-                            binary_count[last_word][this_word] = binary_count[last_word].get(
-                                this_word, 0) + 1
-                    elif ziyuan == 3:
-                        if idx > 1 \
-                                and sentence[0][idx - 1] in words \
-                                and sentence[1][idx - 1] in pinyin2words \
-                                and sentence[0][idx - 1] in pinyin2words[sentence[1][idx - 1]] \
-                                and sentence[0][idx - 2] in words \
-                                and sentence[1][idx - 2] in pinyin2words \
-                                and sentence[0][idx - 2] in pinyin2words[sentence[1][idx - 2]]:
-                            last_word = sentence[0][idx - 1] + sentence[1][idx - 1]
-                            last_last_word = sentence[0][idx - 2] + sentence[1][idx - 2]
-                            if last_last_word not in optional_count:
-                                optional_count[last_last_word] = {}
-                            if last_word not in optional_count[last_last_word]:
-                                optional_count[last_last_word][last_word] = {}
-                            optional_count[last_last_word][last_word][this_word] = optional_count[last_last_word][last_word].get(
-                                this_word, 0) + 1
+        t = threading.Thread(target=count_month, args=(month, ziyuan))
+        t.start()
+        threading_list.append(t)
+
+    for t in threading_list:
+        t.join()
 
     with open(WORD_COUNT, 'w', encoding='utf-8') as f:
         json.dump(count, f, ensure_ascii=False, indent=4)
@@ -90,4 +114,4 @@ if __name__ == '__main__':
     parser.add_argument('-f', '--force', action='store_true', help='是否强制重新统计')
     args = parser.parse_args()
 
-    output_counts(ziyuan=args.ziyuan, force=args.force)
+    preprocess(args.ziyuan, args.force)
